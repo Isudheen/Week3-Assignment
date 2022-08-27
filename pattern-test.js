ac6f70749d613ce561112fedb95e66b780d6f047
Isudheen <61105586+Isudheen@users.noreply.github.com>
2022-08-27 11:40:25 +0530

    const arr1 to let aar1

--- pattern-test.js
+++ pattern-test.js
@@ -1,17 +1,19 @@
 const limit = getInput();
 
-let i, j, arr1 = [], pattern;
- for(i = 0; i < limit; i++) {
-  for(j = 0; j <= i; j++) {
-   arr1[j] = "*";
+let i, j, pattern;
+let arr1 = [];
+for (i = 0; i < limit; i++) {
+  for (j = 0; j <= i; j++) {
+    arr1[j] = '*';
   }
   pattern = arr1.join(' ');
   console.log(pattern);
- }
+  arr1 = [];
+}
 
- function getInput() {
-  const readline = require("readline-sync");
-  console.log("Enter a beautiful limit: ");
+function getInput() {
+  const readline = require('readline-sync');
+  console.log('Enter a beautiful limit: ');
   let ans = readline.question();
   return ans;
- }
+}
