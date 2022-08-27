56ac13b62c2ca66e9f1eea9a90d757f52b1cd451
Isudheen <61105586+Isudheen@users.noreply.github.com>
2022-08-27 13:41:45 +0530

    initial-commit of 4.js

---
+++ 4.js
@@ -0,0 +1,15 @@
+const mark = getInput();
+if (mark >= 50 && mark >= 0 && mark <= 100) {
+  console.log('Passed');
+} else if (mark < 50 && mark >= 0) {
+  console.log('Failed');
+} else {
+  console.log('Invalid input');
+}
+
+function getInput() {
+  const readline = require('readline-sync');
+  console.log('Enter the mark: ');
+  const ans = Number(readline.question());
+  return ans;
+}
