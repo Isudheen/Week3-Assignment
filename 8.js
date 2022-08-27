c1a84c97a9c543e5e4d5ee08ee006d175b2eec75
Isudheen <61105586+Isudheen@users.noreply.github.com>
2022-08-27 23:12:43 +0530

    8.js completed

--- 8.js
+++ 8.js
@@ -1,6 +1,15 @@
 const readline = require(`readline-sync`);
 const limit = getInput();
 
+let i,
+  sum = 0;
+for (i = 1; i <= limit; i++) {
+  if (i % 2 === 1 || i === 1) {
+    sum += i;
+  }
+}
+console.log(`Sum of odd numbers upto ${limit} is : ${sum}`);
+
 function getInput() {
   console.log(`Enter the limit to find sum of odd numbers: `);
   const ans = Number(readline.question());
