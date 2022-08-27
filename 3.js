c6bbbe1fae18f377d0a54737464c337aa78cf576
Isudheen <61105586+Isudheen@users.noreply.github.com>
2022-08-27 12:04:27 +0530

    parseFloat and parseInt added to 3.js

--- 3.js
+++ 3.js
@@ -1,10 +1,19 @@
-console.log('Enter the principal amount:');
-const principal = getInput();
+const readline = require('readline-sync');
 
-console.log();
+console.log('Enter the principal amount: ');
+const principal = parseInt(getInput());
+console.log(principal);
+
+console.log('Enter the interest rate: ');
+const rate = parseFloat(getInput());
+
+console.log('Enter duration: ');
+const time = parseFloat(getInput());
+
+const interest = (principal * rate * time) / 100;
+console.log(`The simple interest is ${interest}`);
 
 function getInput() {
-  const readline = require('readline-sync');
   let num = Number(readline.question());
   return num;
 }
