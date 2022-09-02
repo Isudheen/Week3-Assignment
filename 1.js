63e4afea84c6f83bce62367fd517d19b64468596
Isudheen <61105586+Isudheen@users.noreply.github.com>
2022-09-02 11:26:52 +0530

    1.js update

--- 1.js
+++ 1.js
@@ -1,12 +1,10 @@
 const char = getInput();
 console.log(char);
-console.log(typeof char)
-
-
+console.log(typeof char);
 
 function getInput() {
- const readline = require("readline-sync");
- console.log("Enter a character: ");
- let ans = String(readline.question());
- return ans;
+  const readline = require('readline-sync');
+  console.log('Enter a character: ');
+  let ans = String(readline.question());
+  return ans;
 }
